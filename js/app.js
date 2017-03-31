2df554e0300194ea90345b6b4c75669b5ddc41f7
dubech <franck.dubech@hotmail.com>
2017-03-31 11:22:40 +0200

    Ajout du button suprimer

--- js/app.js
+++ js/app.js
@@ -9,15 +9,15 @@ $( document ).ready(function() {
 		var nom = $('#Name').val();
 		var prenom = $('#Prenom').val()
 		var age = $('#Age').val()
-		$('table').append($("<tr><td>"+nom +"</td><td>"+prenom +"</td><td>"+age +"</td></tr>"));
+		$('table').append($("<tr><td>"+nom +"</td><td>"+prenom +"</td><td>"+age +
+			"</td><td><button id='suprimer'>Suprimer</button></td></tr>"));
 		
 		var person = {'nom':nom , 'prenom': prenom , 'age' : age};
 		annuaire.push(person)
 		console.log(annuaire);
 	})
 
-	$('#Suprimer').click(function(){
-		$('tr').remove()
-
-	})
+		$( "table" ).delegate( '#suprimer', "click", function() {
+  		$( this ).parent().parent().remove()
+  		})
 })
