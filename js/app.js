b9dc7c7c806777bda86b68ed19abfa919599c1ae
dubech <franck.dubech@hotmail.com>
2017-03-31 17:06:57 +0200

    modif du bouton suprimer pour suprimer dans le tableau la console et les entre

--- js/app.js
+++ js/app.js
@@ -11,12 +11,7 @@ $( document ).ready(function() {
 		var age = $('#Age').val();
 		var buttonDel = "<button id='suprimer'>suprimer</button>";
 
-			$("#tab").DataTable().row.add([
-			nom,
-			prenom,
-			age,
-			buttonDel
-			]).draw(false);
+			$("#tab").DataTable().row.add([nom,prenom,age,buttonDel]).draw(false);
 		
 		var person = {'nom':nom , 'prenom': prenom , 'age' : age};
 		annuaire.push(person)
@@ -25,9 +20,13 @@ $( document ).ready(function() {
 		
 	})
 
-		$( "tbody" ).delegate( '#suprimer', "click", function() {
+		$( "#tab" ).on("click", '#suprimer', function() {
+			$("#tab").DataTable().row($(this).parent().parent("tr")).remove().draw();
   		$( this ).parent().parent().remove()
-
+  		var Delentre = $(this).parent().parent().data();
+		annuaire.splice(Delentre, 1);
+		console.log(annuaire);
+	
   		})
  		
  		$(document).ready(function () {
